bc9bc853de08742b997a01091c85ebb6495e96c9
Alelizzt <alejandrolistc@hotmail.com>
2024-10-16 16:19:03 -0500

    Implementing Chat controller

---
+++ src/main/java/com/system/chat/chat/ChatMessage.java
@@ -0,0 +1,19 @@
+package com.system.chat.chat;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ChatMessage {
+
+	private String content;
+	private String sender;
+	private MessageType type;
+}
---
+++ src/main/java/com/system/chat/chat/MessageType.java
@@ -0,0 +1,8 @@
+package com.system.chat.chat;
+
+public enum MessageType {
+
+	CHAT,
+	JOIN,
+	LEAVER
+}
